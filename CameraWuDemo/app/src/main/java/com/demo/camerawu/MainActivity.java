a66f571c2328f05759da54b5f637c35a02cb6e16
dorayo <yomingyo@gmail.com>
2020-05-28 12:35:11 +0800

    相机预览，自定义拍照，自定义拍摄h264视频

---
+++ CameraWuDemo/app/src/main/java/com/demo/camerawu/MainActivity.java
@@ -0,0 +1,61 @@
+package com.demo.camerawu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.demo.camerawu.util.PermissionsChecker;
+
+public class MainActivity extends AppCompatActivity {
+    private PermissionsChecker mPermissionsChecker; // 权限检测器
+
+    Button btnShowView, btnPictures, btnH264Record;
+
+    //定位权限,获取app内常用权限
+    String[] permsLocation = {"android.permission.READ_PHONE_STATE"
+            , "android.permission.ACCESS_COARSE_LOCATION"
+            , "android.permission.ACCESS_FINE_LOCATION"
+            , "android.permission.READ_EXTERNAL_STORAGE"
+            , "android.permission.WRITE_EXTERNAL_STORAGE"
+            , "android.permission.CAMERA"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPermissionsChecker = new PermissionsChecker(MainActivity.this);
+        if (mPermissionsChecker.lacksPermissions(permsLocation)) {
+            ActivityCompat.requestPermissions(MainActivity.this, permsLocation, 1);
+        }
+        btnShowView = findViewById(R.id.btnShowView);
+        btnPictures = findViewById(R.id.btnPictures);
+        btnH264Record = findViewById(R.id.btnH264Record);
+
+        //预览
+        btnShowView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CameraShowActivity.class));
+            }
+        });
+        //拍照
+        btnPictures.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PicturesActivity.class));
+            }
+        });
+        //录制h264
+        btnH264Record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, H264RecordActivity.class));
+            }
+        });
+    }
+}
---
+++ CameraWuDemo/app/src/main/java/com/demo/camerawu/util/AvcEncoder.java
@@ -0,0 +1,189 @@
+package com.demo.camerawu.util;
+
+import android.annotation.SuppressLint;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.os.Environment;
+
+import com.demo.camerawu.H264RecordActivity;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+
+public class AvcEncoder {
+    private final static String TAG = "MeidaCodec";
+
+    private int TIMEOUT_USEC = 12000;
+
+    private MediaCodec mediaCodec;
+    private int m_width;
+    private int m_height;
+    private int m_framerate;
+    public byte[] configbyte;
+
+    private static String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/cameraWuDemo.h264";
+    private BufferedOutputStream outputStream;
+
+
+    @SuppressLint("NewApi")
+    public AvcEncoder(int width, int height, int framerate) {
+
+        m_width = width;
+        m_height = height;
+        m_framerate = framerate;
+
+        MediaFormat mediaFormat = MediaFormat.createVideoFormat("video/avc", width, height);
+        mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar);
+        mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, width * height * 5);
+        mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, m_framerate);
+        mediaFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1);
+        try {
+            mediaCodec = MediaCodec.createEncoderByType("video/avc");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        mediaCodec.configure(mediaFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        mediaCodec.start();
+        createFile();
+    }
+
+    private void createFile() {
+        File file = new File(path);
+        if (file.exists()) {
+            file.delete();
+        }
+        try {
+            outputStream = new BufferedOutputStream(new FileOutputStream(file));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private void StopEncoder() {
+        try {
+            mediaCodec.stop();
+            mediaCodec.release();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isRuning = false;
+
+    //停止录制
+    public void StopThread() {
+        isRuning = false;
+        try {
+            StopEncoder();
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    //开始录制
+    public void StartEncoderThread() {
+        Thread EncoderThread = new Thread(new Runnable() {
+
+            @SuppressLint("NewApi")
+            @Override
+            public void run() {
+                isRuning = true;
+                byte[] input = null;
+                long pts = 0;
+                long generateIndex = 0;
+
+                while (isRuning) {
+                    if (H264RecordActivity.YUVQueue.size() > 0) {
+                        input = H264RecordActivity.YUVQueue.poll();
+                        byte[] yuv420sp = new byte[m_width * m_height * 3 / 2];
+                        NV21ToNV12(input, yuv420sp, m_width, m_height);
+                        input = yuv420sp;
+                    }
+                    if (input != null) {
+                        try {
+                            ByteBuffer[] inputBuffers = mediaCodec.getInputBuffers();
+                            ByteBuffer[] outputBuffers = mediaCodec.getOutputBuffers();
+                            int inputBufferIndex = mediaCodec.dequeueInputBuffer(-1);
+                            if (inputBufferIndex >= 0) {
+                                pts = computePresentationTime(generateIndex);
+                                ByteBuffer inputBuffer = inputBuffers[inputBufferIndex];
+                                inputBuffer.clear();
+                                inputBuffer.put(input);
+                                mediaCodec.queueInputBuffer(inputBufferIndex, 0, input.length, pts, 0);
+                                generateIndex += 1;
+                            }
+
+                            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+                            int outputBufferIndex = mediaCodec.dequeueOutputBuffer(bufferInfo, TIMEOUT_USEC);
+                            while (outputBufferIndex >= 0) {
+                                ByteBuffer outputBuffer = outputBuffers[outputBufferIndex];
+                                byte[] outData = new byte[bufferInfo.size];
+                                outputBuffer.get(outData);
+                                if (bufferInfo.flags == 2) {
+                                    configbyte = new byte[bufferInfo.size];
+                                    configbyte = outData;
+                                } else if (bufferInfo.flags == 1) {
+                                    byte[] keyframe = new byte[bufferInfo.size + configbyte.length];
+                                    System.arraycopy(configbyte, 0, keyframe, 0, configbyte.length);
+                                    System.arraycopy(outData, 0, keyframe, configbyte.length, outData.length);
+
+                                    outputStream.write(keyframe, 0, keyframe.length);
+                                } else {
+                                    outputStream.write(outData, 0, outData.length);
+                                }
+
+                                mediaCodec.releaseOutputBuffer(outputBufferIndex, false);
+                                outputBufferIndex = mediaCodec.dequeueOutputBuffer(bufferInfo, TIMEOUT_USEC);
+                            }
+
+                        } catch (Throwable t) {
+                            t.printStackTrace();
+                        }
+                    } else {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+        EncoderThread.start();
+
+    }
+
+    /**
+     * Generates the presentation time for frame N, in microseconds.
+     */
+    private long computePresentationTime(long frameIndex) {
+        return 132 + frameIndex * 1000000 / m_framerate;
+    }
+
+
+    public void NV21ToNV12(byte[] nv21, byte[] nv12, int width, int height) {
+        if (nv21 == null || nv12 == null) return;
+        int framesize = width * height;
+        int i = 0, j = 0;
+        System.arraycopy(nv21, 0, nv12, 0, framesize);
+        for (i = 0; i < framesize; i++) {
+            nv12[i] = nv21[i];
+        }
+        for (j = 0; j < framesize / 2; j += 2) {
+            nv12[framesize + j - 1] = nv21[j + framesize];
+        }
+        for (j = 0; j < framesize / 2; j += 2) {
+            nv12[framesize + j] = nv21[j + framesize - 1];
+        }
+    }
+}
